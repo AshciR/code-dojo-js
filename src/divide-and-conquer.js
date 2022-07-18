3644a7c0d5159f4e8167d1ca5c261a47da64b358
Richard Walker <richard.walker.90@gmail.com>
2022-07-18 19:49:22 -0400

    Added Max recurive solution

--- src/divide-and-conquer.js
+++ src/divide-and-conquer.js
@@ -5,11 +5,11 @@
  */
 const sumTheArray = (nums) => {
     // Find the base case: if array is empty or 1 element
-    if(nums.length === 0){
+    if (nums.length === 0) {
         return 0;
     }
 
-    if(nums.length === 1){
+    if (nums.length === 1) {
         return nums[0];
     }
 
@@ -17,4 +17,30 @@ const sumTheArray = (nums) => {
     return nums[0] + sumTheArray(nums.slice(1));
 }
 
-console.log(sumTheArray([1,2,3]));
+/**
+ * Find the max number in a list, 0 if it's empty
+ * @param {number[]}nums
+ * @return {number}
+ */
+const maxNumber = (nums) => {
+
+    // Find the base case: if array is empty or 1 element
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    // 2nd base case: we wanna compare 2 values
+    // return the larger of the two
+    if (nums.length === 2) {
+        return (nums[0] > nums[1]) ? nums[0] : nums[1];
+    }
+
+    const maxNumberFromTheRestOfTheArray = maxNumber(nums.slice(1));
+    return (nums[0] > maxNumberFromTheRestOfTheArray) ? nums[0] : maxNumberFromTheRestOfTheArray;
+}
+
+console.log(maxNumber([20, 3, 15, 2, 400, 1, 1]));
