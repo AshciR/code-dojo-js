b42312dbcc56ecc2b9e49163199083c3148bad4b
Richard Walker <richard.walker.90@gmail.com>
2022-07-18 20:27:29 -0400

    Added quicksort

--- src/divide-and-conquer.js
+++ src/divide-and-conquer.js
@@ -43,4 +43,39 @@ const maxNumber = (nums) => {
     return (nums[0] > maxNumberFromTheRestOfTheArray) ? nums[0] : maxNumberFromTheRestOfTheArray;
 }
 
-console.log(maxNumber([20, 3, 15, 2, 400, 1, 1]));
+/**
+ * Uses quick sort algorithm
+ * @param {number[]}nums
+ */
+const quickSort = (nums) => {
+
+    // Base case 1: if it's empty
+    if(nums.length === 0){
+        return [];
+    }
+
+    // Base case 2: if it's 1 element
+    if(nums.length === 1){
+        return [nums[0]];
+    }
+
+    // Base case 3: if it's 2 elements, swap them
+    if(nums.length === 2){
+        return (nums[0] > nums[1]) ? [nums[0], nums[1]] : [nums[1], nums[0]];
+    }
+
+    // Otherwise, find a pivot, then place all the numbers less than it one 1 side
+    // and the numbers greater than it on the other side.
+    // Then call the divided problem
+
+    const pivot = nums[Math.floor(nums.length/2)]; // Using the middle element as the pivot
+
+    const lessThanPivot = nums.filter(num => num < pivot);
+    const greaterThanPivot = nums.filter(num => num > pivot);
+
+    // Combined the arrays
+    return [...quickSort(lessThanPivot), pivot, ...quickSort(greaterThanPivot)];
+
+}
+
+console.log(quickSort([20, 3, 15, 2, 400, 1, 27]));
